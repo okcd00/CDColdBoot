2c65efa9fd11e1e85ce4aea49e630a3857768138
Dian Chen <okcd00@qq.com>
2021-07-29 15:27:19 +0800

    Update: modify some details in git/conda scripts.

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,11 +1,5 @@
-if [ -d "/home/chendian/my_conda_envs/" ]; then
-    echo "Dir my_conda_env exists."
-else
-    mkdir ~/my_conda_envs
-fi
-
 if [ -n "$1" ]; then
-    conda create --prefix="~/my_conda_envs/$1" python=3.7.10
+    conda create -n $1 python=3.7.10
 else
-    conda create --prefix="~/my_conda_envs/chendian" python=3.7.10
+    conda create -n chendian python=3.7.10
 fi
--- fix_git_fatal_ssl_connect.sh
+++ fix_git_fatal_ssl_connect.sh
@@ -1,2 +1,5 @@
 git config --global http.sslVerify false
 git config --global --add remote.origin.proxy ""
+git config --global http.lowSpeedLimit 0
+git config --global http.lowSpeedTime 999999
+git config http.postBuffer 524288000
