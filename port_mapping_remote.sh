a07f98fc8aa8b96b6e290cd3d083258dde6d3fb9
Dian Chen <okcd00@users.noreply.github.com>
2021-08-12 10:37:29 +0800

    Update port_mapping_remote.sh

--- port_mapping_remote.sh
+++ port_mapping_remote.sh
@@ -1,5 +1,5 @@
 if [ $# != 3 ]; then
-    echo "USAGE: $0 <host> <local_port> <remote_port>"
+    echo "USAGE: $0 <host> <remote_port> <local_port>"
 else
-    ssh $1 -N -f -L 127.0.0.1:$2:127.0.0.1:$3
+    ssh $1 -N -f -L 127.0.0.1:$3:127.0.0.1:$2
 fi
