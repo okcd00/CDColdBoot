ab001131f6881de2c84944f28405a4ab21fda652
Dian Chen <okcd00@users.noreply.github.com>
2021-08-12 10:42:19 +0800

    Update start_jupyterlab_server.sh
    Use the jupyter in current env to setup the jupyter server.

--- start_jupyterlab_server.sh
+++ start_jupyterlab_server.sh
@@ -1,1 +1,1 @@
-sudo /usr/local/anaconda2/envs/py3.6_tf1.15/bin/jupyter serverextension enable --py jupyterlab --sys-prefix 
+sudo `where jupyter` serverextension enable --py jupyterlab --sys-prefix 
