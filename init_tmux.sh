317b7c573ac45749fbfc457651b3fe98a47e7f77
Dian Chen <okcd00@users.noreply.github.com>
2021-12-27 11:04:16 +0800

    Update: add alias for tmux initial.

--- init_tmux.sh
+++ init_tmux.sh
@@ -1,3 +1,4 @@
+# prepare tmux env
 cd ~
 git clone https://gitee.com/okcd00/tmux 
 mv ./tmux ./.tmux
@@ -5,3 +6,16 @@ mkdir ./.tmux/plugins
 git clone https://gitee.com/okcd00/tpm ~/.tmux/plugins/tpm
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
+
+# add `tm` alias in ~/.bashrc
+alias_str="alias tm='tmux a -t chendian'"
+if 
+grep -q "alias tm" ~/.bashrc
+then
+echo "alias ${alias_str} exists"
+else
+# sed `/匹配字段/i加上想要插入到匹配字段前一行的内容` 文件名
+# sed `/匹配字段/a加上想要插入到匹配字段后一行的内容` 文件名
+sed -i "/alias l='ls -CF'/a\${alias_str}" ~/.bashrc
+echo "append ${alias_str} in bashrc"
+fi
