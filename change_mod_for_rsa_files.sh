bd8e57722222e872b992403a68ffa9c830e62eb9
Dian Chen <okcd00@users.noreply.github.com>
2022-02-09 14:21:10 +0800

    Update: for windows
    + `git clone https://github.com/PowerShell/openssh-portable.git`
    + `cd openssh-portable/contrib/win32/openssh`
    + `.\FixUserFilePermissions.ps1 -Confirm:$false` in PowerShell
      + `Set-ExecutionPolicy RemoteSigned` if there are permission issues.

--- change_mod_for_rsa_files.sh
+++ change_mod_for_rsa_files.sh
@@ -1,3 +1,6 @@
 chmod 755 ~/.ssh/  
 chmod 600 ~/.ssh/id_rsa ~/.ssh/id_rsa.pub  
 chmod 644 ~/.ssh/known_hosts ~/.ssh/config
+
+# for Windows VSCode:
+# https://zixizixi.cn/windows_vscode_ssh_error_bad-owner-or-permissions
