ebf6aaff7ce66fafe94971d55c8474fa355688f5
Dian Chen <okcd00@users.noreply.github.com>
2022-02-26 18:00:56 +0800

    Fix: remove extra `[]` in conditions.

--- change_pip_source.sh
+++ change_pip_source.sh
@@ -1,11 +1,11 @@
 if [ -n "$1" ]; then
-  if [[ ${1} = "douban" ]]; then
+  if [ ${1} = "douban" ]; then
     pip config set global.index-url http://pypi.douban.com/simple/ 
-  elif [[ ${1} = "ustc" ]]; then
+  elif [ ${1} = "ustc" ]; then
     pip config set global.index-url https://pypi.mirrors.ustc.edu.cn/simple/
-  elif [[ ${1} = "aliyun" ]]; then
+  elif [ ${1} = "aliyun" ]; then
     pip config set global.index-url http://mirrors.aliyun.com/pypi/simple/ 
-  elif [[ ${1} = "tsinghua" ]]; then
+  elif [ ${1} = "tsinghua" ]; then
     pip config set global.index-url http://pypi.tuna.tsinghua.edu.cn/simple/
   else
     echo "Invalid arg: should be one from [douban|ustc|aliyun]"
