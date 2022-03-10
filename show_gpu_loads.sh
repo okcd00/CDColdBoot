580ec37b296bb26fe1b7bea98746ca66d54f76be
Dian Chen <okcd00@qq.com>
2022-03-10 11:14:05 +0800

    Update: add a switch for toggle color in gpu_loads.

--- show_gpu_loads.sh
+++ show_gpu_loads.sh
@@ -1,17 +1,34 @@
 # loads=`gpustat | awk '{print $8}'`
+with_color=0
 loads_str=""
+
+if [ -n "$1" ]; then
+    if [ ${1} = "--color" ]; then
+        with_color=1  
+    fi
+fi
 
 for num in $( gpustat | awk '{print $8}' )
 do
-    if [ $num -gt 75 ]; then
+    if [ $num -gt 50 ]; then
 	# echo "×"
-	loads_str=$loads_str"\033[31mO\033[0m"
-    elif [ $num -gt 50 ]; then
-        # echo "O"
-        loads_str=$loads_str"\033[33mO\033[0m"
+	if [ $with_color -eq 1 ]; then
+            loads_str=$loads_str"\033[31m■\033[0m"
+	else
+            loads_str=$loads_str"■"
+	fi
+    elif [ $num -gt 25 ]; then
+        if [ $with_color -eq 1 ]; then
+            loads_str=$loads_str"\033[33m▄\033[0m"
+        else
+            loads_str=$loads_str"▄"
+        fi
     else
-        # echo "○"	    
-	loads_str=$loads_str"\033[32mO\033[0m"
+        if [ $with_color -eq 1 ]; then
+            loads_str=$loads_str"\033[32m□\033[0m"
+        else
+            loads_str=$loads_str"□"
+        fi
     fi
 done
 echo $loads_str
