cae4d817629bee7e9075a1aff7272fbc126066b1
Dian Chen <okcd00@qq.com>
2022-03-10 10:46:23 +0800

    Update: show gpu loads in color.

--- show_gpu_loads.sh
+++ show_gpu_loads.sh
@@ -1,1 +1,17 @@
-gpustat | awk '{print $8}' | awk 'BEGIN{cdsum=0}{cdsum+=1}END{print cdsum}'
+# loads=`gpustat | awk '{print $8}'`
+loads_str=""
+
+for num in $( gpustat | awk '{print $8}' )
+do
+    if [ $num -gt 75 ]; then
+	# echo "×"
+	loads_str=$loads_str"\033[31mO\033[0m"
+    elif [ $num -gt 50 ]; then
+        # echo "O"
+        loads_str=$loads_str"\033[33mO\033[0m"
+    else
+        # echo "○"	    
+	loads_str=$loads_str"\033[32mO\033[0m"
+    fi
+done
+echo $loads_str
