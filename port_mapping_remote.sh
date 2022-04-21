a3de1564aa71227ca1599c58ab1250abe239a5bb
Dian Chen <okcd00@users.noreply.github.com>
2022-04-21 14:35:02 +0800

    Update port_mapping_remote.sh
    Allow single port number for remote port mapping.

--- port_mapping_remote.sh
+++ port_mapping_remote.sh
@@ -1,5 +1,7 @@
-if [ $# != 3 ]; then
-    echo "USAGE: $0 <host> <remote_port> <local_port>"
-else
+if [ $# == 2 ]; then
+    ssh $1 -N -f -L 127.0.0.1:$2:127.0.0.1:$2
+elif [ $# == 3 ]; then
     ssh $1 -N -f -L 127.0.0.1:$3:127.0.0.1:$2
+else
+    echo "USAGE: $0 <host> <remote_port> <local_port>"
 fi
