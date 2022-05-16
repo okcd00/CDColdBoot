2b6d6528c041580eeaac2a190a8de84b0788db91
Dian Chen <okcd00@users.noreply.github.com>
2022-05-16 21:38:06 +0800

    Update show_gpu_loads.sh

--- show_gpu_loads.sh
+++ show_gpu_loads.sh
@@ -10,22 +10,27 @@ fi
 
 for num in $( gpustat | awk '{print $8}' )
 do
-    if [ $num -gt 50 ]; then
-	# echo "×"
-	if [ $with_color -eq 1 ]; then
-            loads_str=$loads_str"\033[31m[X]\033[0m"
-	else
+    if [ $num -gt 75 ]; then
+    if [ $with_color -eq 1 ]; then
+            loads_str=$loads_str"\033[31m[◍]\033[0m"
+    else
             loads_str=$loads_str"[X]"
-	fi
+    fi
+    elif [ $num -gt 50 ]; then
+    if [ $with_color -eq 1 ]; then
+            loads_str=$loads_str"\033[31m[◕]\033[0m"
+    else
+            loads_str=$loads_str"[X]"
+    fi
     elif [ $num -gt 25 ]; then
         if [ $with_color -eq 1 ]; then
-            loads_str=$loads_str"\033[33m[-]\033[0m"
+            loads_str=$loads_str"\033[33m[◑]\033[0m"
         else
             loads_str=$loads_str"[-]"
         fi
     else
         if [ $with_color -eq 1 ]; then
-            loads_str=$loads_str"\033[32m[O]\033[0m"
+            loads_str=$loads_str"\033[32m[◌]\033[0m"
         else
             loads_str=$loads_str"[O]"
         fi
