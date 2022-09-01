ae32cecc2c87ad20402f1af5033f8dca39a990da
Dian Chen <okcd00@users.noreply.github.com>
2022-09-01 23:04:00 +0800

    Update kill_processes.sh
    Allow kill processes with longer than 7 digits.

--- kill_processes.sh
+++ kill_processes.sh
@@ -1,5 +1,5 @@
 if [ -n "$1" ]; then
-	ps -ef | grep $1 | grep -v grep | cut -c 9-15 | xargs kill -9
+	ps -ef | grep $1 | grep -v grep | cut -c 9-16 | xargs kill -9
 else
 	echo "a target string is required"
 fi
