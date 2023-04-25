2b2bb3b55237b31af0845fc204711f0ad58046ac
okcd00 <okcd00@qq.com>
2023-04-25 17:13:02 +0800

    Update: A script for pip-ssl-errors.

---
+++ fix_pip_ssl_error.sh
@@ -0,0 +1,3 @@
+pip config set global.index-url http://pypi.tuna.tsinghua.edu.cn/simple/
+pip config set global.trusted-host pypi.tuna.tsinghua.edu.cn
+echo "Windows 环境的报错，需要关闭网络和 Internet 设置中的手动设置代理开关" 
